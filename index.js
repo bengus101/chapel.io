0a9831f35ba2b6e1f20420f5b99c88695f7e99ea
bengus101 <bgustafson29@yahoo.com>
2016-04-24 22:35:08 -0700

    Added root route & port listen to index.js

--- index.js
+++ index.js
@@ -5,3 +5,15 @@ var db = require('./models');
 
 var app = express();
 app.set('view engine', 'ejs');
+app.use(ejsLayouts);
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(__dirname + '/static'));
+
+app.get('/', function(req, res) {
+	res.render('index');
+});
+
+var port = 3000;
+app.listen(port, function() {
+	console.log('Port ' + port + ' straight flexin');
+});
