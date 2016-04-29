8d439525892aa0224e325b2b3f4598efd9241a8c
bengus101 <bgustafson29@yahoo.com>
2016-04-28 18:55:23 -0700

    Can get a gif to appear, but only by hard-coding the url into the code. Still working on this issue.

--- static/main.js
+++ static/main.js
@@ -7,17 +7,25 @@ $('#log-out').click(function() {
 });
 	
 $('#reaction-gif').click(function() {
-	console.log('Gif button clicked!')
+	console.log('Gif button clicked!');
 	var tags = $('#search-tags').val();
-	var url = 'http://replygif.net/api/gifs?tag=' + tags + '&tag-operator=and&api-key=39YAprx5Yi'
-	
+	var url = 'http://replygif.net/api/gifs?tag=' + tags + '&tag-operator=and&api-key=39YAprx5Yi';
+	var response = [{id: '', url: '', file: 'http://replygif.net/i/1536.gif'}];
+	console.log(response);
+
+	response.forEach(function(data) {
+		var img = $('<img src="' + data.file + '">');
+		$('.display').append(img);
+		
+	});
+
 	$.ajax({
 		type: 'GET',
 		url: url
 	}).done(function(data) {
-		console.log(data.length, data);
+		// console.log(data.length, data);
 		data.forEach(function(image) {
-			console.log(image.url);
+			console.log(image.file);
 		})
 	});
 	
