a38d4189051491930621f8224441287e5e9ab935
bengus101 <bgustafson29@yahoo.com>
2016-04-28 19:54:57 -0700

    Gifs now show up on the page.

--- static/main.js
+++ static/main.js
@@ -8,25 +8,31 @@ $('#log-out').click(function() {
 	
 $('#reaction-gif').click(function() {
 	console.log('Gif button clicked!');
+	$('.display').empty();
 	var tags = $('#search-tags').val();
 	var url = 'http://replygif.net/api/gifs?tag=' + tags + '&tag-operator=and&api-key=39YAprx5Yi';
-	var response = [{id: '', url: '', file: 'http://replygif.net/i/1536.gif'}];
+	var response = [];
 	console.log(response);
-
-	response.forEach(function(data) {
-		var img = $('<img src="' + data.file + '">');
-		$('.display').append(img);
-		
-	});
 
 	$.ajax({
 		type: 'GET',
 		url: url
 	}).done(function(data) {
-		// console.log(data.length, data);
-		data.forEach(function(image) {
-			console.log(image.file);
-		})
+		console.log(data);
+		data.forEach(function(image, index) {
+		var newObj = {
+			id: image.id,
+			url: image.url,
+			file: image.file
+		};
+		var img = $('<img src="' + newObj.file + '">');
+		$('.display').append(img);
+		});
+	});
+
+	response.forEach(function(data) {
+		
+		
 	});
 	
 });
